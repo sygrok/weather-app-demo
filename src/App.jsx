8a927351b6d76a5a357f6e6c6b1ef097a4b2cc56
Berkay Anduv <sygrok@hotmail.com>
2023-12-05 17:18:35 +0300

    FirstCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 function App() {
   return (
     <>
-      <h1>Start</h1>
+      <section className="app">
+        <div className="overlay">
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name="city" placeholder="Enter City" />
+              <button>f</button>
+            </div>
+
+            <div className="section section__ temperature">
+              <div className="icon">
+                <h3>London, GB</h3>
+                <img src="dsa" alt="weatherIcon" />
+                <h3>Cloudy</h3>
+              </div>
+              <div className="temperature">
+                <h1>34 C</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
