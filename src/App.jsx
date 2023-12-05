3ef045308be844cac048e7ddd74d33eae5afbfbc
Berkay Anduv <sygrok@hotmail.com>
2023-12-05 19:36:48 +0300

    DataForApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import Description from "./components/Description";
+import { getFormattedWeatherData } from "./WeatherService";
 
 function App() {
+  const [weather, setWeather] = useState("paris");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(weather);
+      setWeather(data);
+    };
+
+    fetchWeatherData();
+  }, []);
   return (
     <>
       <div className="app">
@@ -8,20 +20,22 @@ function App() {
           <div className="container">
             <div className="section section__inputs">
               <input type="text" name="city" placeholder="Enter City" />
-              <button>f</button>
+              <button>°F</button>
             </div>
 
             <div className="section section__temperature">
               <div className="icon">
-                <h3>London, GB</h3>
-                <img src="dsa" alt="weatherIcon" />
-                <h3>Cloudy</h3>
+                <h3>
+                  {weather.name} {weather.country}
+                </h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
               </div>
               <div className="temperature">
-                <h1>34 C</h1>
+                <h1>{weather.temp?.toFixed()}°C</h1>
               </div>
             </div>
-            <Description />
+            <Description weather={weather} />
           </div>
         </div>
       </div>
---
+++ src/WeatherService.js
@@ -0,0 +1,38 @@
+const API_KEY = "e1894962d85814e048359cffda95b540";
+
+const makeIconUrl = (iconId) => {
+  return `https://openweathermap.org/img/wn/${iconId}.png`;
+};
+
+export const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconUrl(icon),
+    // icon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
